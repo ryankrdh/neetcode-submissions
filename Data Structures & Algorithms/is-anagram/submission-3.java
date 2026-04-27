53aa2516a35f2dd9a86c4e6a4cec069f58f21d24
Ryan (Hyunmin) Kim <ryankrdh@gmail.com>
2026-04-26 17:09:49 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            count.put(s.charAt(i), count.getOrDefault(s.charAt(i), 0) + 1);
+            count.put(t.charAt(i), count.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// TIME: TIME: O(n + m) = O(n): n is the length of s, and m is the length of t.
+// ALSO: O(n + 26) = O(n): second loop only ever runs at most 26 times. constants are dropped
+
+// SPACE: O(1): Hash map can hold at most 26 keys
