a89d3bc87acc86b8f3039ab05989756fa0068905
Ryan (Hyunmin) Kim <ryankrdh@gmail.com>
2026-04-28 23:07:04 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
+
+// TIME: O(m * n log n). m is the number of strings and n is the length of the longest string.
+// SPACE: O(m * n)
